03c0c3e9a75e7a99db948e9c5a5ff9177b7afda8
JagadeeshGeerla <jagadeesh.geerla@gmail.com>
2018-11-23 14:58:25 +0530

    Connecting to Mongo and Filtering

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const db = mongoose.connect('mongodb://localhost/bookAPIdb'); //Creates db if doesn't exist
+const Book = require('./models/bookModel');
 
 const app = express();
-
 const port = process.env.port || 3000;
 
 const bookRouter = express.Router();
 
 bookRouter.route('/Books')
     .get(function(req, res) {
-        var responseJson = {hello: "this is my API"};
-
-        res.json(responseJson);
+        //var query = req.query;
+        //console.log(query);
+        var query = {};
+        if(req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function(err, books) {
+            if(err) {
+                console.log(err);
+            }
+            else
+                res.json(books);
+        });
     });
 
 app.use('/api', bookRouter);
