0a3cb98105761a8e0d9074ffa9a886e73950d621
JagadeeshGeerla <jagadeesh.geerla@gmail.com>
2018-11-23 16:17:23 +0530

    just commiting Rough, for testing

--- RoughWork.js
+++ RoughWork.js
@@ -1,5 +1,8 @@
 {
-    "title" : "Animal Farm",
-    "genre" : "Fictional",
-    "author" : "George Orwell"
+    "read": false,
+    "_id": "5bf7d7b791e27e43043d710a",
+    "title": "Animal Farm",
+    "genre": "Fictional",
+    "author": "George Orwell",
+    "__v": 0
 }
